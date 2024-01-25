77986858ff5fe2648166231e4f7b3362c8e3a5d0
Dibik <dibik@openg2p.org>
2024-01-26 01:00:19 +0530

    G2P-1756: theme odoo title debranding

--- g2p_theme/static/src/js/g2p_window_title.js
+++ g2p_theme/static/src/js/g2p_window_title.js
@@ -1,12 +1,11 @@
-// /** @odoo-module **/
+/** @odoo-module **/
 
-// import { patch } from "@web/core/utils/patch";
-// import { WebClient } from "@web/webclient/webclient";
+import {patch} from "@web/core/utils/patch";
+import {WebClient} from "@web/webclient/webclient";
 
-// patch(WebClient.prototype, {
-//     setup() {
-//         var self = this;
-//         self._super.apply(this, arguments);
-//         this.title.setParts({zopenerp: "OpenG2P"});
-//     },
-// });
+patch(WebClient.prototype, {
+    setup() {
+        super.setup();
+        this.title.setParts({zopenerp: "OpenG2P"});
+    },
+});
